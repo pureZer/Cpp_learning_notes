7318d800244a6b9f4edc962962ad02c3768ef3c1
pureZer <997207556@qq.com>
2018-04-05 13:56:06 +0800

    第二章

---
+++ 第二章/2.1进入C++.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//使用cout进行C++输出
+	cout << "Come up and C++ me some time.";
+	
+	//控制符endl
+	cout << "The Good, the";
+	cout << "Bad, ";
+	cout << "and the Ukulele";
+	cout << endl;
+	
+	//换行符
+	cout << "What's next?\n"; 
+	return 0;	
+} 
