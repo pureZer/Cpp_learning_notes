d90de5c1fd0425e68f3d27324127bc1355c13737
pureZer <335908768@qq.com>
2018-04-05 14:35:44 +0800

    第三章

--- 第三章/3.1简单变量.cpp
+++ 第三章/3.1简单变量.cpp
@@ -46,6 +46,39 @@ int main()
 	sam = sam - 1;
 	sue = sue - 1;
 	cout << "Sam has " << sam << " dollars and Sue has " << sue;
-	cout << " dollars deposited." << endl << "Lucky Sue!" << endl;
+	cout << " dollars deposited." << endl << "Lucky Sue!" << endl<<endl;
+	
+	int chest = 42;
+	int waist = 0x42;
+	int inseam = 042;
+	cout << "Monsieur cuts a striking figure!\n";
+	cout << "chest = " << chest << " (42 int decimal)\n";
+	cout << "waist = " << waist << " (0x42 int hex)\n";
+	cout << "inseam = " << inseam << " (042 in toctal)\n\n";
+	
+	chest = 42;
+	waist = 42;
+	inseam = 42;
+	cout << "Monsieur cuts a striking figure!" << endl;
+	cout << "chest = " << chest << " (decimal for 42)" << endl;
+	cout << hex;
+	cout << "waist = " << waist << " (hex for 42)" << endl;
+	cout << oct;
+	cout << "inseam = " << inseam << " (octal for 42)" << endl << endl;
+	
+	char ch = 'M';
+	int i = ch;
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+	
+	cout << "Add one to the character code:" << endl;
+	ch = ch + 1;
+	i = ch;
+	cout << "The ASCII code for " << ch << " is " << i << endl;
+	
+	cout << "Displaying char ch using cout.put(ch): ";
+	cout.put(ch);
+	
+	cout.put('!');
+	cout << endl << "Done" << endl; 
 	return 0;
 } 
