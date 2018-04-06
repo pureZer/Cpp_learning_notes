ffc764bcdb88a36570d1b66b9e6868fcf079dced
pureZer <335908768@qq.com>
2018-04-06 22:27:04 +0800

    第三章

--- 第三章/3.4C++算数运算符.cpp
+++ 第三章/3.4C++算数运算符.cpp
@@ -38,6 +38,20 @@ int main()
 	int debt = 7.2E12;
 	cout << "tree = " << tree << endl;
 	cout << "guess = " << guess << endl;
-	cout << "debt = " << debt << endl;
+	cout << "debt = " << debt << endl << endl;
+	
+	int auks, bats, coots;
+	auks = 19.99 + 11.99;
+	
+	bats = (int)19.99 + (int)11.99;
+	coots = int(19.99) + int(11.99);
+	cout << "auks = " << auks << ", bats = " << bats;
+	cout << ", coots = " << coots << endl;
+	
+	char ch = 'Z';
+	cout << "The code for " << ch << " is ";
+	cout << int(ch) << endl;
+	cout << "Yes, the code is ";
+	cout << static_cast<int>(ch) << endl; 
 	return 0;
 }
