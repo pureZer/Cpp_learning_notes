1219f026f69a9afd81c9325fd09e1041de2954f1
pureZer <335908768@qq.com>
2018-04-06 22:14:23 +0800

    第三章

--- 第三章/3.4C++算数运算符.cpp
+++ 第三章/3.4C++算数运算符.cpp
@@ -14,6 +14,30 @@ int main()
 	cout << "hats + heads = " << hats + heads << endl;
 	cout << "hats - heads = " << hats - heads << endl;
 	cout << "hats * heads = " << hats * heads << endl;
-	cout << "hats / heads = " << hats / heads << endl; 
+	cout << "hats / heads = " << hats / heads << endl << endl; 
+	
+	cout << "Ingeter division: 9/5 = " << 9 / 5 << endl;
+	cout << "Floating-point division: 9.0/5.0 = ";
+	cout << 9.0 / 5.0 << endl;
+	cout << "Mixed division: 9.0/5 = " << 9.0 / 5 << endl;
+	cout << "double constants: 1e7/9.0 = ";
+	cout << 1.e7 / 9.0 << endl;
+	cout << "float constants: 1e7f/9.0f = ";
+	cout << 1.e7f / 9.0f << endl << endl;
+	
+	const int Lbs_stn = 14;
+	int lbs;
+	cout << "Enter your weight in pounds: ";
+	cin >> lbs;
+	int stone = lbs / Lbs_stn;
+	int pounds = lbs % Lbs_stn;
+	cout << lbs << " pounds are " << stone << " stone, " << pounds << " pounds(s)" << endl << endl;
+	
+	float tree = 3;
+	int guess(3.9832);
+	int debt = 7.2E12;
+	cout << "tree = " << tree << endl;
+	cout << "guess = " << guess << endl;
+	cout << "debt = " << debt << endl;
 	return 0;
 }
