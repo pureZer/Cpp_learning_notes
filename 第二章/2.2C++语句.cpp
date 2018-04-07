582850defbfe4beb8d44a49372781956a05de67e
pureZer <335908768@qq.com>
2018-04-07 17:02:28 +0800

    第二章

---
+++ 第二章/2.2C++语句.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int carrots;
+	carrots = 25;
+	cout << "I have ";
+	cout << carrots;
+	cout << "carrots.";
+	cout << endl;
+	carrots = carrots - 1;
+	cout << "Crunch, crunch. Now I have " << carrots << " carrots." << endl;
+	
+	return 0;
+}
